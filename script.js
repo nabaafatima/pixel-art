e00b957fa7afb9b31bc127fea04956b5b572bc0f
nabaafatima <nabaafatima7097@gmail.com>
2026-06-01 15:33:57 +0500

    it colors now

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const grid = document.getElementById("grid");
+const item = document.getElementById("item");
+const color = document.getElementById("color");
+const eraser = document.getElementById("clear");
+let isDrawing = false;
+grid.addEventListener('mousedown', () => isDrawing = true);
+window.addEventListener('mouseup', () => isDrawing = false);
 for (let i = 0; i < 960; i++) {
     const newdiv = document.createElement("div");
     newdiv.className='item';
-    grid.appendChild(newdiv); 
-}
+    grid.appendChild(newdiv);
+    newdiv.addEventListener('mousedown', function() {
+        this.style.backgroundColor = color.value;
+    });
+    newdiv.addEventListener('mouseenter', function() {
+        if (isDrawing) {
+            this.style.backgroundColor = color.value;
+        }
+    });
 
+}
+function clear () {
+    item.value = eraser.value;
+}
